89e6fe17c2ac5ea9bfc8c7c975b8855788daadb0
vaishuvenky <vaishuvenkat2830@gmail.com>
2022-07-22 19:09:11 +0530

    anu done her coding

--- src/org/gitsamp/Sample.java
+++ src/org/gitsamp/Sample.java
@@ -3,7 +3,7 @@ package org.gitsamp;
 public class Sample {
 public static void main(String[] args) {
 	int a=10;
-	System.out.println("GIT sample Program");
 	
+	System.out.println("GIT sample Program");
 }
 }
